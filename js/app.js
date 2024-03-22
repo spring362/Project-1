b9e655f6d4745cb90d96481addf1b816e3d8a10d
talhaa71 <143561482+talhaa71@users.noreply.github.com>
2024-03-21 23:43:58 -0500

    Added finction in js, added media image, added sebred text
    I added a logo image of reddit to my page, I added a validation function in js for my email, I added backup fonts to my external fonts, and added some margins form my image and subred text

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 function myFunction() {
-  alert("Thank you for joining!");
+  var emailVal = validateEmail();
+  if (emailVal) {
+    alert("Thank you for joining!");
+  }
+}
+function validateEmail() {
+      var emailInput = document.getElementById('email');
+      var email = emailInput.value;
+      var emailRegex = /^\S+@\S+\.\S+$/;
+      if (!emailRegex.test(email)) {
+        alert('Please enter a valid email address.');
+        return false; 
+      }
+      return true;
 }
